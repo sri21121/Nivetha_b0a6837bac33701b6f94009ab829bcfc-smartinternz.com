6528500c44ed4bebe2efcf975e98e9e15b886be3
sri21121 <144714885+sri21121@users.noreply.github.com>
2023-09-11 22:20:32 +0530

    Update main (2).py

--- unit 1 chellange/main (2).py
+++ unit 1 chellange/main (2).py
@@ -1,10 +1,8 @@
-def isleap_year(year):
- if (year%4==0 and year%100!=0) or year%400==0:
-  return True
+def fact_rec(n):
+ if n==0 or n==1:
+   return 1
  else:
-   return False
-year=int(input("Enter a year : "))
-if isleap_year(year):
-    print('{} is a leap year.'.format(year))
-else:
-    print('{} is not a leap year.'.format(year))
+  return n*fact_rec(n-1)
+number=int(input("enter the number:"))
+res=fact_rec(number)
+print("The factorial of {} is {}.".format(number,res))
