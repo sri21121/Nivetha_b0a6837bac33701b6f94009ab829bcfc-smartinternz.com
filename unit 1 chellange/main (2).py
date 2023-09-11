ac1519feb0362e9875248815826469b930e59a6f
sri21121 <144714885+sri21121@users.noreply.github.com>
2023-09-11 21:59:03 +0530

    Add files via upload

---
+++ unit 1 chellange/main (2).py
@@ -0,0 +1,10 @@
+def isleap_year(year):
+ if (year%4==0 and year%100!=0) or year%400==0:
+  return True
+ else:
+   return False
+year=int(input("Enter a year : "))
+if isleap_year(year):
+    print('{} is a leap year.'.format(year))
+else:
+    print('{} is not a leap year.'.format(year))
