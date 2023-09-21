3add2ec6b3c144d964ec2527930b4ba2b78b874b
sri21121 <144714885+sri21121@users.noreply.github.com>
2023-09-21 20:44:37 +0530

    Update main (4).py

--- unit2 chellange/main (4).py
+++ unit2 chellange/main (4).py
@@ -1,19 +1,38 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket")
+class BankAccount:
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
+  def _init_(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ${}. New balance:${}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid deposit amount. Please deposit a positive  amount.")
 
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdrew ${}. New balance: ${}".format(amount,
+                                                    self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+  def display_balance(self):
+    print("Account balance for {} (account #{}): ${}".format(
+        self.__account_holder_name, self.__account_number,
+        self.__account_balance))
+
+
+# Create an instance of the BankAccount class
+account = BankAccount(account_number="987654321",
+                      account_holder_name="Nivi",
+                      initial_balance=8000.0)
+# Test deposit and withdrawal functionalty
+#account.display_balance()
+#account.deposit(800.0)
+#account.withdraw(200.0)
+account.display_balance()
